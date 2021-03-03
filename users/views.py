ccb03bed06c06b01c1c4a7dc2f09ee79c7f681fe
OlyaPinchuk <olha5959@gmail.com>
2021-03-03 19:17:52 +0200

    Initial commit.

---
+++ users/views.py
@@ -0,0 +1,45 @@
+import os
+import json
+from django.shortcuts import render
+
+from .models import User
+
+# Create your views here.
+
+name = 'Max'
+names = ['Jack', 'Ann']
+db_path = os.path.join('users', 'db.json')
+
+
+
+def home(request):
+    # print(request)
+    return render(request, 'users/home.html', {'names': names})
+
+
+def users(request):
+    # file = open(db_path, 'r')
+    # data = json.load(file)
+    # file.close()
+
+    with open(db_path, 'r') as file:
+        users_list = [User(**item) for item in json.load(file)]
+    return render(request, 'users/users.html', {'users': users_list})
+
+# 1 варіант отр даних
+# def create_user(request, id, name, age):
+#     print(id)
+#     print(name)
+#     print(age)
+#     return render(request, 'users/users.html')
+
+# 2 варіант отр даних
+
+
+def create_user(request, **kwargs):
+    print(kwargs)
+    with open(db_path, 'w') as file:
+        json.dump(kwargs, file)
+    with open(db_path, 'r') as file:
+        user = User(**json.load(file))
+    return render(request, 'users/users.html', {'user': user})
